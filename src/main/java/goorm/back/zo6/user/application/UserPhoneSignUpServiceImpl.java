8b500f9eebec6cce00d6ffe1f1ad5a14a6e723ff
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-10 21:45:51 +0900

    UserService signUpWithPhone 메서드 이동

---
+++ src/main/java/goorm/back/zo6/user/application/UserPhoneSignUpServiceImpl.java
@@ -0,0 +1,36 @@
+package goorm.back.zo6.user.application;
+
+import goorm.back.zo6.reservation.application.command.ReservationCommandService;
+import goorm.back.zo6.user.domain.Role;
+import goorm.back.zo6.user.domain.User;
+import goorm.back.zo6.user.domain.UserRepository;
+import goorm.back.zo6.user.dto.request.SignUpRequest;
+import goorm.back.zo6.user.dto.response.SignUpResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserPhoneSignUpServiceImpl implements UserPhoneSignUpService {
+
+    private final UserRepository userRepository;
+    private final UserValidator userValidator;
+    private final PasswordEncoder passwordEncoder;
+    private final ReservationCommandService reservationCommandService;
+    private final PhoneValidService phoneValidService;
+
+    @Override
+    @Transactional
+    public SignUpResponse signUpWithPhone(SignUpRequest request) {
+        userValidator.validatePhone(request.phone());
+        User user = userRepository.save(User.singUpUser(request.email(), request.name(), passwordEncoder.encode(request.password()), request.phone(), Role.of("USER")));
+
+        reservationCommandService.linkReservationByPhone(request.phone());
+
+        phoneValidService.removeVerifiedPhone(request.phone());
+
+        return SignUpResponse.from(user);
+    }
+}
